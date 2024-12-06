a98737eef84637716974821e66120da71d6f7a9a
Priminant <simiso.nkonyane9120@gmail.com>
2024-12-07 00:15:41 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Portfolio – Simiso Nkonyane</title>
+  <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<body>
+  <header>
+     <nav>
+      <div class="logo">Simiso Andria</div>
+      <ul class="nav-links">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#resume">Resume</a></li>
+        <li><a href="#blog">Blog</a></li>
+        <li><a href="contact.php">Contact</a></li>
+      </ul>
+    </nav>
+    <div class="hero-content">
+      <h1>HELLO! <br>I Am Simiso Nkonyane</h1>
+      <p>
+        I’m a developer with a passion for creating innovative digital solutions.
+        My expertise includes web development, application design, and more.
+      </p>
+      <div class="hero-buttons">
+        <a href="#portfolio" class="btn">View Work</a>
+        <a href="#contact" class="btn">Hire Me</a>
+      </div>
+    </div>
+  </header>
+
+</body>
+</html>
